eec01a27a4e3e22183cea06fec207327fbe19458
unknown <kemahbarry@gmail.com>
2022-08-17 03:41:40 +0100

    updated api routing

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -1,10 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  const Id = req.query.tokenId
-  const name = `CrytoDevs #${Id}`
+  const id = req.query.tokenId
+  const name = `CrytoDevs #${id}`
   const description = "CryptoDevs is an NFT collection for web3 developers"
-  const imageUrl = `https://github.com/LearnWeb3DAO/NFT-Collection/blob/main/my-app/public/cryptodevs/${Number(tokenId) - 1}.svg`
+  const imageUrl = `https://github.com/LearnWeb3DAO/NFT-Collection/blob/main/my-app/public/cryptodevs/${Number(id) - 1}.svg`
 
   return res.json({
     name: name,
